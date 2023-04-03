b80842d2649f0ff4b3ceaf54220558d33935d956
Lukas Dresel <Lukas-Dresel@users.noreply.github.com>
2023-04-02 18:36:57 -0700

    more linter shit

--- pwn/src/ethpwn/currency_utils.py
+++ pwn/src/ethpwn/currency_utils.py
@@ -1,27 +1,44 @@
+'''
+Utilities for dealing with the different units of currency in the Ethereum ecosystem.
+'''
 
 from web3 import Web3
 from .global_context import context
 
-def parse_wei(s):
-    if s.strip().endswith('ether'):
-        return Web3.to_wei(s.strip()[:-5].strip(), 'ether')
-    elif s.strip().endswith('gwei'):
-        return Web3.to_wei(s.strip()[:-4].strip(), 'gwei')
+def parse_wei(value_string):
+    '''
+    Parse a string representing into a wei value. The string can be in ether, gwei, or wei.
+    If no unit is specified, it is assumed to be wei.
+    '''
+    value_string = value_string.lower()
+    if value_string.strip().endswith('ether'):
+        return Web3.to_wei(value_string.strip()[:-5].strip(), 'ether')
+    elif value_string.strip().endswith('eth'):
+        return Web3.to_wei(value_string.strip()[:-3].strip(), 'ether')
+    elif value_string.strip().endswith('gwei'):
+        return Web3.to_wei(value_string.strip()[:-4].strip(), 'gwei')
     else:
-        return int(s)
+        return int(value_string)
 
+# pylint: disable=redefined-outer-name
 def wei(ether=None, gwei=None, wei=None):
-    sum = 0
+    '''
+    Convert some amount of ether, gwei, and/or wei to wei. Returns the sum of all values so you can
+    do `wei(ether=1, gwei=1)` to get 1 ether and 1 gwei in wei.
+    '''
+    _sum = 0
     if ether is not None:
-        sum += context.w3.to_wei(ether, 'ether')
+        _sum += context.w3.to_wei(ether, 'ether')
     if gwei is not None:
-        sum += context.w3.to_wei(gwei, 'gwei')
+        _sum += context.w3.to_wei(gwei, 'gwei')
     if wei is not None:
-        sum += wei
-    return sum
+        _sum += wei
+    return _sum
 
 def ether(wei):
+    '''Convert wei to ether'''
     return context.w3.from_wei(wei, 'ether')
 
 def gwei(wei):
+    '''Convert wei to gwei'''
     return context.w3.from_wei(wei, 'gwei')
--- pwn/src/ethpwn/global_context.py
+++ pwn/src/ethpwn/global_context.py
@@ -1,14 +1,18 @@
+'''
+Global context accessible from anywhere in the ethpwn package.
+'''
 
 import contextlib
 import logging
 
 from web3 import Web3, middleware
-import web3
-from web3.gas_strategies.rpc import rpc_gas_price_strategy
-from web3.gas_strategies.time_based import fast_gas_price_strategy, construct_time_based_gas_price_strategy
-
+from web3.gas_strategies.time_based import construct_time_based_gas_price_strategy
 
 class Web3Context:
+    '''
+    A context holding global state used by ethpwn.
+    '''
+    # pylint: disable=invalid-name
     def __init__(self, w3=None, from_addr=None, private_key=None, log_level=logging.WARNING):
         self.w3 = w3
 
@@ -19,6 +23,10 @@ class Web3Context:
 
     @property
     def default_from_addr(self):
+        '''
+        Get the default from address as set or via the default wallet
+        '''
+        # pylint: disable=import-outside-toplevel
         from .config.wallets import get_wallet
         if self._default_from_addr is None:
             return get_wallet(None).address
@@ -30,6 +38,10 @@ class Web3Context:
 
     @property
     def default_signing_key(self):
+        '''
+        Get the default signing key
+        '''
+        # pylint: disable=import-outside-toplevel
         from .config.wallets import get_wallet
         if self._default_signing_key is None:
             return get_wallet(None).private_key
@@ -42,6 +54,9 @@ class Web3Context:
     # if the log_level is changed, update the logger
     @property
     def log_level(self):
+        '''
+        Get the log level of the logger
+        '''
         return logging.getLevelName(self.logger.level)
 
     @log_level.setter
@@ -51,41 +66,64 @@ class Web3Context:
         if not logging.getLogger().handlers:
             self.logger.addHandler(logging.StreamHandler())
 
+    def connect(self, url, can_fail=False, **kwargs):
+        '''
+        Connect to an Ethereum node via HTTP/HTTPS, Websocket, or IPC depending on the URL scheme.
+        '''
+        if url.startswith('http'):
+            return self.connect_http(url, can_fail=can_fail, **kwargs)
+        elif url.startswith('ws'):
+            return self.connect_websocket(url, can_fail=can_fail, **kwargs)
+        else:
+            return self.connect_ipc(url, can_fail=can_fail)
+
     def connect_http(self, url, can_fail=False, **kwargs):
+        '''
+        Connect to a remote Ethereum node via HTTP/HTTPS
+        '''
         self.w3 = Web3(Web3.HTTPProvider(url, **kwargs))
         if not self.w3 or not self.w3.is_connected():
             if can_fail:
-                self.logger.warning(f'Could not connect to {url}')
-                return
+                self.logger.warning('Could not connect to %s', url)
+                return False
             else:
-                raise Exception(f'Could not connect to {url}')
+                raise ValueError(f'Could not connect to {url}')
         else:
-            self.logger.info(f'Connected to {url}')
+            self.logger.info('Connected to %s', url)
             self._configure_w3()
+            return True
 
     def connect_ipc(self, path='/home/eth/.ethereum/geth.ipc', can_fail=False):
+        '''
+        Connect to a local Ethereum node via IPC
+        '''
         self.w3 = Web3(Web3.IPCProvider(path))
         if not self.w3 or not self.w3.is_connected():
             if can_fail:
-                self.logger.warning(f'Could not connect to {path}')
-                return
+                self.logger.warning('Could not connect to %s', path)
+                return False
             else:
-                raise Exception(f'Could not connect to {path}')
+                raise ValueError(f'Could not connect to {path}')
         else:
-            self.logger.info(f'Connected to {path}')
+            self.logger.info('Connected to %s', path)
             self._configure_w3()
+            return True
 
     def connect_websocket(self, url, can_fail=False, **kwargs):
+        '''
+        Connect to an Ethereum node via WebSockets
+        '''
         self.w3 = Web3(Web3.WebsocketProvider(url, **kwargs))
         if not self.w3 or not self.w3.is_connected():
             if can_fail:
-                self.logger.warning(f'Could not connect to {url}')
-                return
+                self.logger.warning('Could not connect to %s', url)
+                return False
             else:
-                raise Exception(f'Could not connect to {url}')
+                raise ValueError(f'Could not connect to {url}')
         else:
-            self.logger.info(f'Connected to {url}')
+            self.logger.info('Connected to %s', url)
             self._configure_w3()
+            return True
 
     def _configure_w3(self):
         self.w3.eth.set_gas_price_strategy(
@@ -101,15 +139,30 @@ class Web3Context:
         self.w3.middleware_onion.add(middleware.simple_cache_middleware)
 
     def pessimistic_gas_price_estimate(self):
+        '''
+        Estimate the gas price for a transaction. This is a pessimistic estimate that will
+        overestimate the gas price by a factor of 2. This should be good enough to mostly
+        ensure that the transaction will be mined in a reasonable amount of time.
+        '''
         return context.w3.eth.generate_gas_price() * 2
 
     def pessimistic_transaction_cost(self, gas_used_estimate):
+        '''
+        Estimate the cost of a transaction. This is a pessimistic estimate that will
+        overestimate the gas price by a factor of 2. This should be good enough to mostly
+        ensure that the transaction will be mined in a reasonable amount of time.
+        '''
         return self.pessimistic_gas_price_estimate() * gas_used_estimate
 
 context: Web3Context = Web3Context()
 
 @contextlib.contextmanager
 def with_local_context(**kwargs):
+    '''
+    Temporarily set the global context to a new context. Will restore the old context when the
+    context manager exits.
+    '''
+    # pylint: disable=invalid-name,global-statement
     global context
     old_context = context
     context = Web3Context(**kwargs)
