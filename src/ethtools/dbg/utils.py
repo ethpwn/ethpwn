18a035f6ded1bb4c0f7daeefe76432cc0887e966
Lukas Dresel <Lukas-Dresel@users.noreply.github.com>
2023-07-24 00:18:16 -0700

    cleanup

--- src/ethtools/dbg/utils.py
+++ src/ethtools/dbg/utils.py
@@ -6,6 +6,7 @@ import struct
 import os
 
 from pathlib import Path
+from ..pwn.utils import ChainName
 
 RED_COLOR = "\033[31m"
 GREEN_COLOR = "\033[32m"
@@ -28,18 +29,8 @@ STRIKETHROUGH = "\u0336"
 
 FOUR_BYTE_URL = "https://raw.githubusercontent.com/ethereum-lists/4bytes/master/signatures/{}"
 
-# Enum for the different types of chain
-# that are supported by the tool
-class ChainName:
-    MAINNET = 1
-    SEPOLIA = 11155111
-    AVALANCHE = 43114
 
 SUPPORTED_CHAINS = [ChainName.MAINNET, ChainName.SEPOLIA, ChainName.AVALANCHE]
-
-def to_snake_case(s: str) -> str:
-    s = s.replace('-', '_')
-    return ''.join(['_' + c.lower() if c.isupper() else c for c in s]).lstrip('_')
 
 def get_terminal_size():
     """Return the current terminal size."""
@@ -64,24 +55,6 @@ def get_terminal_size():
             return tty_rows, tty_columns
         except OSError:
             return 600, 100
-
-def get_chainid(chain_name):
-    if chain_name == "mainnet":
-        return 1
-    elif chain_name == "sepolia":
-        return 11155111
-    else:
-        raise Exception(f"Unknown chain name {chain_name}")
-
-def get_chain_name(id):
-    if id == 1:
-        return "mainnet"
-    elif id == 11155111:
-        return "sepolia"
-    elif id == 43114:
-        return "avalanche"
-    else:
-        raise Exception("Unknown chain id")
 
 def load_cmds_history():
     target_file = Path().home() / ".config" / "ethtools" / ".ethdbg_history"
--- src/ethtools/pwn/utils.py
+++ src/ethtools/pwn/utils.py
@@ -5,7 +5,7 @@ from .global_context import context
 
 @functools.lru_cache(maxsize=1024)
 def normalize_contract_address(address) -> str:
-    """Normalize a contract address"""
+    """Normalize a contract address. This ensures all addresses are checksummed and have the 0x prefix."""
     if not address:
         return None
 
@@ -20,7 +20,16 @@ def normalize_contract_address(address) -> str:
 
     return Web3.to_checksum_address(address)
 
+
+def to_snake_case(s: str) -> str:
+    s = s.replace('-', '_')
+    return ''.join(['_' + c.lower() if c.isupper() else c for c in s]).lstrip('_')
+
+
 def show_diff(a, b, show_old_new=False):
+    '''
+    Show a nice `rich` table of the diff between two objects using `deepdiff`.
+    '''
     from deepdiff import DeepDiff
 
     from rich import print
@@ -42,10 +51,30 @@ def show_diff(a, b, show_old_new=False):
             table.add_row(diff_type, diff_path, diff_value['diff'])
     print(table)
 
+
+# Enum for the different types of chain
+# that are supported by the tool
+class ChainName:
+    MAINNET = 1
+    SEPOLIA = 11155111
+    AVALANCHE = 43114
+
+
+def get_chainid(chain_name):
+    if chain_name == "mainnet":
+        return 1
+    elif chain_name == "sepolia":
+        return 11155111
+    else:
+        raise Exception(f"Unknown chain name {chain_name}")
+
+
 def get_chain_name(id):
     if id == 1:
         return "mainnet"
     elif id == 11155111:
         return "sepolia"
+    elif id == 43114:
+        return "avalanche"
     else:
         raise Exception("Unknown chain id")
