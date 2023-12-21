1d7123a5f4df999d8abef762644d857564b772bc
degrigis <degrigis@ucsb.edu>
2023-12-21 12:39:43 -0800

    get_evm_from_txn
    Signed-off-by: degrigis <degrigis@ucsb.edu>

--- src/ethpwn/ethlib/evm_analyzer.py
+++ src/ethpwn/ethlib/evm_analyzer.py
@@ -10,9 +10,23 @@ def get_evm_at_block(block_number:int, **kwargs) -> EVMAnalyzer:
     '''
     return EVMAnalyzer.from_block_number(context.w3, block_number)
 
-def get_evm_at_block_and_tx_index(block_number:int, txindex:int, **kwargs) -> EVMAnalyzer:
+
+def get_evm_at_txn(txn:int) -> EVMAnalyzer:
     '''
-    Return an EVMAnalyzer instance for the given block number and transaction index.
+    Return an EVMAnalyzer instance for the given transaction hash.
     '''
-    import ipdb; ipdb.set_trace()
-    return EVMAnalyzer.from_block_and_txindex(context.w3, block_number, txindex)
+    w3txn = context.w3.eth.get_transaction(txn)
+    block_number = w3txn['blockNumber']
+    w3block = context.w3.eth.get_block(block_number)
+
+    a = EVMAnalyzer.from_block_number(context.w3, block_number)
+
+    print(f"Applying transactions up to {txn}")
+    # apply the transactions up to the one we want 
+    for t in w3block.transactions:
+        if t.hex() == txn:
+            break
+        else:
+            a.next_transaction()
+    
+    return a
